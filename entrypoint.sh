0e41030c0fd0b567c87122b8d3ef026c1d59bf9d
Katherine Eaton <ktmeaton@gmail.com>
2026-02-12 16:45:18 -0700

    checkpoint: selenium tests for OIDC and SSO

--- entrypoint.sh
+++ entrypoint.sh
@@ -22,7 +22,18 @@ tty_wrapper() {
 if [ "${MODE}" = 'TESTING' ]; then
     # Expand out the args first, otherwise only the first arg is captured
     args="${@}"
-    tty_wrapper "perl t/test_fixture.pl --carpalways -v $args"
+    if [[ $args == "--interactive" ]]; then
+        echo "No testing arguments were given, setting up interactive mode."
+
+        echo "Patching database and building node modules."
+        echo "--version" > ~/.proverc
+        tty_wrapper "perl t/test_fixture.pl --dumpupdatedfixture"
+        rm -f ~/.proverc
+
+        tail -f /dev/null
+    else
+        tty_wrapper "perl t/test_fixture.pl --carpalways -v $args"
+    fi
     exit $?
 fi
 
