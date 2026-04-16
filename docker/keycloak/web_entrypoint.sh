ef7e31ae53d97af272643328f110a7cb7ca94c87
Katherine Eaton <ktmeaton@gmail.com>
2026-04-16 12:52:31 -0600

    BFF-AFIRMS sgn-449.0 (#17)
    * Bring up to version sgn-449.0. Includes support for intercropping; use of obsoleted stocks in trials; fixes to the trait search so that treatments will not be shown; a cosmetic tweak to the cvterm detail page; adds a sliding trial title such that it is always visible; fixes a bug in the job submission system that required parameters that were not necessary.
    
    * db: fix patch 203 that has infinite hang in postgres 17 and 18
    
    * keycloak: make entrypoint more reusable with dynamic file var
    
    * jbrowse: fix missing 4.1.13 version and upgrade to 4.1.15
    
    * ci: free up disk space for deployment, dockerhub, and test
    
    * treeimprovementbase: remove problematic stock page
    
    ---------
    
    Co-authored-by: Lukas Mueller <lam87@cornell.edu>

--- docker/keycloak/web_entrypoint.sh
+++ docker/keycloak/web_entrypoint.sh
@@ -3,12 +3,13 @@
 set -e
 
 # Configure custom realms
-echo "Initializing Breedbase realm"
+echo "Initializing custom realm"
 if [[ $KC_HOSTNAME ]]; then
-    sed -i -E "s|(\"frontendUrl\":).*|\1 \"$KC_HOSTNAME\",|g" /opt/keycloak/data/import/breedbase-realm.json
+    sed -E "s|(\"frontendUrl\":).*|\1 \"$KC_HOSTNAME\",|g" /opt/keycloak/data/import/${REALM_FILE} > /tmp/${REALM_FILE}
+else
+    cp /opt/keycloak/data/import/${REALM_FILE} /tmp/${REALM_FILE}
 fi
-/opt/keycloak/bin/kc.sh import --file=/opt/keycloak/data/import/breedbase-realm.json --override=false
-
+/opt/keycloak/bin/kc.sh import --file=/tmp/${REALM_FILE} --override=false
 
 # Start server
-/opt/keycloak/bin/kc.sh start-dev
+/opt/keycloak/bin/kc.sh start
